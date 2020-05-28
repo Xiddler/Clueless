4d5d28a0246337d5875738977e666b07f5b193c5
Donagh <xiddler@gmail.com>
2020-05-28 19:11:06 +0100

    correct / incorrect seem to be working. Class works. Next publish to www.donaghmccarthy.ie

--- clueless-webapp/run.py
+++ clueless-webapp/run.py
@@ -10,20 +10,28 @@ app = Flask(__name__)
 app.config.from_object(Config) # Secret key in config.py
 # secret_key = app.config['SECRET_KEY']
 
-def constraints():
-    ''' Set up the constraints for this game '''
-    num = 2
-    level = "Easy"
-    rule = "Ends in letter w"
-    time = "4 minutes"
-    game = f"No: {num}  |  Level: {level}  |  Constraint: {rule}  |  Time allowed: {time}"
-    return game
+class Constraints:
+
+
+    def __init__(self, num, level, rule, time):
+
+        self.num = num
+        self.level = level
+        self.rule = rule
+        self.time = time
+        # self.game = f"# {self.num} Level: {self.level} Rule: {self.rule} Time allowed: {self.time}"
+        self.game = "# {0} Level: {1} Rule: {2} Time allowed: {3}".format( self.num, self.level, self.rule, self.time)
+
+constraints = Constraints(2, "Easy", "Ends in a w", "4 mins")
+num = constraints.num
+level = constraints.level
+rule = constraints.rule
+time = constraints.time
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     # Welcome page with constraints
-    game = constraints()
-    return render_template('index.html', game=game)
+    return render_template('index.html', num=num, level=level, rule=rule, time=time)
 
 
 
@@ -31,12 +39,11 @@ def index():
 def game():
     print("in the /game route now")
     # The present game
-    game = constraints() # call the puzzle of the day method
-    print(constraints())
     # loaded when the player presses enter in any of the input boxes
     if request.method == 'POST': 
         data = request.form # in key:value pairs
         print(data)
+        print(num)
         word_holder = [] # initialise list of words entered by user
         word_a = data.get('aword') # data['aword'] requires an initial value in the input field
         word_b = data.get('bword')
@@ -75,20 +82,23 @@ def game():
         # check if all words inserted are in the words table (ie the 'dictionary')
         all_items = [item[0] for item in all_words[0:88280]] # make into a regular list
         incorrect = []
+        correct = []
         for word in word_holder:
             if word not in all_items and word != "None":
                 incorrect.append(word) # incorrect words added to list for display on the webpage
             else:
-                print(word) # correct words printed to terminal
+                correct.append(word)
+                # print(word) # correct words printed to terminal
+        print(incorrect)
+        print(correct)
         conn.close()	
         print("Closed database successfully")
 
-        return render_template('game.html', word_a=word_a, word_b=word_b, word_c=word_c, word_d=word_d, word_e=word_e, word_f=word_f, word_g=word_g, word_h=word_h, word_i=word_i, word_j=word_j, word_k=word_k, word_l=word_l, word_m=word_m, word_n=word_n, word_o=word_o, word_p=word_p, word_q=word_q, word_r=word_r, word_s=word_s, word_t=word_t, word_u=word_u, word_v=word_v, word_w=word_w, word_x=word_x, word_y=word_y, word_z=word_z, incorrect=incorrect, num=num, level=level, rule=rule, time=time, game=game)
+        return render_template('game.html', word_a=word_a, word_b=word_b, word_c=word_c, word_d=word_d, word_e=word_e, word_f=word_f, word_g=word_g, word_h=word_h, word_i=word_i, word_j=word_j, word_k=word_k, word_l=word_l, word_m=word_m, word_n=word_n, word_o=word_o, word_p=word_p, word_q=word_q, word_r=word_r, word_s=word_s, word_t=word_t, word_u=word_u, word_v=word_v, word_w=word_w, word_x=word_x, word_y=word_y, word_z=word_z, word_holder=word_holder, correct=correct,  incorrect=incorrect,  rule=rule, time=time )
 
+     # Input fields are blank if loaded by pressing the PLAY! button in index.html
     else:
-         # Input fields are blank if loaded by pressing the PLAY! button in index.html
-        return render_template('game.html', game=game) 
-        # return redirect(url_for('/game'))
+        return render_template('game.html', num=num, level=level, rule=rule, time=time) 
 
 if __name__ == '__main__':
     app.run(host ='0.0.0.0', port=5000, debug=True) # '0.0.0.0' allows browsing from other devices on the lan.
